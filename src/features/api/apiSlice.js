4a1d7a215b788ce96ca6a1e75024f8d8fec2b60c
Michelle <michelledng7@gmail.com>
2023-01-02 23:38:59 +1100

    add update/add/delete mutation in apiSlice and get back useMutation hooks

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,9 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5500/' }), //query the server
+	tagTypes: ['Todos'],
 	endpoints: (builder) => ({
 		getTodos: builder.query({
 			query: () => '/todos',
+			transformResponse: (response) => response.sort((a, b) => b.id - a.id),
+			providesTags: ['Todos'],
 		}),
 		addTodo: builder.mutation({
 			query: (newTodo) => ({
@@ -13,13 +16,15 @@ export const apiSlice = createApi({
 				method: 'POST',
 				body: newTodo,
 			}),
+			invalidatesTags: ['Todos'],
 		}),
 		updateTodo: builder.mutation({
 			query: (updateTodo) => ({
 				url: `/todos/${updateTodo.id}`,
-				method: 'PATCH',
+				method: 'PUT',
 				body: updateTodo,
 			}),
+			invalidatesTags: ['Todos'],
 		}),
 		deleteTodo: builder.mutation({
 			query: ({ id }) => ({
@@ -27,12 +32,14 @@ export const apiSlice = createApi({
 				method: 'DELETE',
 				body: id,
 			}),
+			invalidatesTags: ['Todos'],
 		}),
-		//tranformResponse:
-		//providesTags:
 	}),
 });
 
-export const { useGetTodosQuery } = apiSlice;
-
-export default apiSlice;
+export const {
+	useGetTodosQuery,
+	useDeleteTodoMutation,
+	useUpdateTodoMutation,
+	useAddTodoMutation,
+} = apiSlice;
--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,4 +1,9 @@
-import { useGetTodosQuery } from '../api/apiSlice';
+import {
+	useGetTodosQuery,
+	useAddTodoMutation,
+	useUpdateTodoMutation,
+	useDeleteTodoMutation,
+} from '../api/apiSlice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
@@ -6,10 +11,13 @@ import { useState } from 'react';
 const TodoList = () => {
 	const [newTodo, setNewTodo] = useState('');
 	const { data, error, isError, isLoading, isSuccess } = useGetTodosQuery();
+	const [addTodo] = useAddTodoMutation();
+	const [updateTodo] = useUpdateTodoMutation();
+	const [deleteTodo] = useDeleteTodoMutation();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		//addTodo
+		addTodo({ userId: 1, title: newTodo, completed: false });
 		setNewTodo('');
 	};
 
@@ -35,7 +43,28 @@ const TodoList = () => {
 	if (isLoading) {
 		content = <p>Loading</p>;
 	} else if (isSuccess) {
-		content = JSON.stringify(data); //to json
+		//content = JSON.stringify(data); //to json
+		content = data.map((todo) => {
+			console.log(todo.id);
+			return (
+				<article key={todo.id}>
+					<div className='todo'>
+						<input
+							type='checkbox'
+							checked={todo.completed}
+							id={todo.id}
+							onChange={() =>
+								updateTodo({ ...todo, completed: !todo.completed })
+							}
+						/>
+						<label htmlFor={todo.id}>{todo.title}</label>
+					</div>
+					<button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+						<FontAwesomeIcon icon={faTrash} />
+					</button>
+				</article>
+			);
+		});
 	} else if (isError) {
 		content = <p>error</p>;
 	}
